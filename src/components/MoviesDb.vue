5e9126d16910e00b42c3d4c5df2c507b00571913
Kirsten Markley <markleyk@otc.edu>
2022-05-03 08:03:57 -0500

    added the movies

--- src/components/MoviesDb.vue
+++ src/components/MoviesDb.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>
+   <div>
         <h1>Upcoming Movies</h1>
-      
+        <div v-for="movie in movies" :key=movie.id>
+           
+            {{movie.title}}
+            {{movie.id}}
+
+        </div>
     </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default{
-    name:"MoviesDb"
+    name:"MoviesDb",
+    data(){
+        return {
+            movies: []
+        }
+    }, 
+    mounted()
+    {
+        axios.get("")
+        .then( (response)=> {
+            this.movies = response.data.results;
+        })
+    }
 }
 </script>
